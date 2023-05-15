2dabef671e9119ce78973edfcace742ac83857b0
evhenii.belianinov <evhenii.belianinov@paysera.net>
2023-05-15 18:05:42 +0300

    Directories were fixed

--- src/main/java/data/Directories.java
+++ src/main/java/data/Directories.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 public interface Directories extends Config {
     @DefaultValue("../Selenium/")
     String mainCore();
-    @DefaultValue("${mainCore}/src/main/resources/")
+    @DefaultValue("${mainCore}/src/main/resources")
     String mainResources();
     @DefaultValue("${mainResources}/data/")
     String data();
