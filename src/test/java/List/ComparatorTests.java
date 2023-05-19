b1a5979dba66e120430e32a28669c2640fb38b4d
evhenii.belianinov <evhenii.belianinov@paysera.net>
2023-05-19 09:54:02 +0300

    ComparatorTests

--- src/test/java/List/ComparatorTests.java
+++ src/test/java/List/ComparatorTests.java
@@ -2,10 +2,7 @@ package List;
 
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class ComparatorTests {
 
@@ -45,7 +42,7 @@ public class ComparatorTests {
                 if (o1 < o2) {
                     return 1;
                 } else if (o1 > o2) {
-                    return  -1;
+                    return -1;
                 } else {
                     return 0;
                 }
@@ -53,12 +50,55 @@ public class ComparatorTests {
         });
         System.out.println(numbers);
     }
- }
 
- class StringLenghtComparator implements Comparator<String> {
+    @Test(description = "Objects comparing")
+    void objectsComparing() {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person(3, "Bob"));
+        people.add(new Person(2, "Katy"));
+        people.add(new Person(1, "Mike"));
 
-     @Override
-     public int compare(String o1, String o2) {
+        Collections.sort(people, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                if (o1.getId() > o2.getId()) {
+                    return 1;
+                } else if (o1.getId() < o2.getId()) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+        System.out.println(people);
+    }
+
+    @Test(description = "Comparable interface")
+    void comparable() {
+        List<Person> peopleList = new ArrayList<>();
+        Set<Person> peopleSet = new TreeSet<>();
+
+        addElements(peopleList);
+        addElements(peopleSet);
+        Collections.sort(peopleList);
+
+        System.out.println(peopleList);
+        System.out.println(peopleSet);
+    }
+
+    private static void addElements(Collection collections) {
+        collections.add(new Person(5, "Bob"));
+        collections.add(new Person(3, "Tom"));
+        collections.add(new Person(2, "Katy"));
+        collections.add(new Person(4, "George"));
+        collections.add(new Person(1, "Testing"));
+    }
+}
+
+class StringLenghtComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
          /*
             o1 > o2 -> 1;
             o1 < o2 -> -1;
@@ -69,25 +109,80 @@ public class ComparatorTests {
             compare(3, 3) -> 0;
           */
 
-         if (o1.length() > o2.length()) {
-             return 1;
-         } else if (o1.length() < o2.length()) {
-             return -1;
-         } else {
-             return 0;
-         }
-     }
- }
+        if (o1.length() > o2.length()) {
+            return 1;
+        } else if (o1.length() < o2.length()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
 
- class BackwardsIntegerComparator implements Comparator<Integer> {
-     @Override
-     public int compare(Integer o1, Integer o2) {
-         if (o1 < o2) {
-             return 1;
-         } else if (o1 > o2) {
-             return  -1;
-         } else {
-             return 0;
-         }
-     }
- }
+class BackwardsIntegerComparator implements Comparator<Integer> {
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        if (o1 < o2) {
+            return 1;
+        } else if (o1 > o2) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
+
+class Person implements Comparable<Person> {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (id != person.id) return false;
+        return Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        if (this.name.length() > o.getName().length()) {
+            return 1;
+        } else if (this.name.length() < o.getName().length()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
