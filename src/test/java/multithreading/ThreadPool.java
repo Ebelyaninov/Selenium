5ee08ddc0bd3d0a82007d0c9bd1e069378e06672
evhenii.belianinov <evhenii.belianinov@paysera.net>
2023-05-22 11:53:23 +0300

    ThreadPool

--- src/test/java/multithreading/ThreadPool.java
+++ src/test/java/multithreading/ThreadPool.java
@@ -1,4 +1,39 @@
 package multithreading;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class ThreadPool {
+    public static void main(String[] args){
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        for (int i = 0; i < 5; i++) {
+            executorService.submit(new Work(i));
+        }
+        executorService.shutdown();
+        System.out.println("All tasks submitted");
+        try {
+            executorService.awaitTermination(60000, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class Work implements Runnable {
+    private int id;
+
+    public Work(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Work " + id + " was completed");
+    }
 }
